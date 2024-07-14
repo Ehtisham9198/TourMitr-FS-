239b38c8e62b04c4f964ff0aace465d1ab0c9047
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2024-07-14 19:21:41 +0530

    Your commit message

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { registerController, loginController, ratingController, fetchRatingController, recommendationController } = require('../controllers/authController');
+
+router.post('/register', registerController);
+router.post('/login', loginController);
+router.post('/rating', ratingController);
+router.get('/rating/:placeId', fetchRatingController);
+router.post('/recommendations', recommendationController);
+
+module.exports = router;
