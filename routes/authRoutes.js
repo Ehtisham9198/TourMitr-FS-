7ec65617d7a2dd208b3be52a17fe533b6bc19111
Ehtisham mohd <b522035@iiit-bh.ac.in>
2024-07-14 18:35:29 +0530

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {registerControler,loginControler} = require('../controllers/authController');
-
-
-router.post('/register',registerControler)
-router.post('/login',loginControler)
-
-module.exports = router;
